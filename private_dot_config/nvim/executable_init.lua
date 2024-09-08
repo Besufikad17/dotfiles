104d900025049b632d77c45d31a727eb73c6cb37
Besufikad <besumicheal@gmail.com>
2024-09-09 01:47:11 +0500

    dotfiles added using chezmoi

---
+++ private_dot_config/nvim/executable_init.lua
@@ -0,0 +1,3 @@
+require("bes.options")
+require("bes.keymaps")
+require("bes.plugins")
---
+++ private_dot_config/polybar/executable_launch.sh
@@ -0,0 +1,16 @@
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# polybar -q main -c "~/.config/polybar/config.ini"
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
+
